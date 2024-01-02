f168ee4117837f8a661c6f2147395cf1ee887904
AhmedMaqbul <ahmedmaqbul.ma@gmail.com>
2024-01-02 15:05:43 +0530

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using iTextSharp.text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Rectangle = System.Windows.Shapes.Rectangle;
+
+namespace SnakeGameApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public const int SnakeSquareSize = 20;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DrawGameArea()
+        {
+            bool doneDrwaingBackround = false;
+            int nextX = 0, nextY = 0;
+            int rowCounter = 0;
+            bool nextIsOdd = false;
+
+            while (doneDrwaingBackround == false)
+            {
+                Rectangle rectangle = new Rectangle
+                {
+                    Width = SnakeSquareSize,
+                    Height = SnakeSquareSize,
+                    Fill = nextIsOdd ? Brushes.White : Brushes.Black
+                };
+
+                GameArea.Children.Add(rectangle);
+                Canvas.SetTop(rectangle, nextY);
+                Canvas.SetLeft(rectangle, nextX);
+
+                nextIsOdd = !nextIsOdd;
+                nextX += SnakeSquareSize;
+
+                if(nextX >= GameArea.ActualWidth)
+                {
+                    nextX = 0;
+                    nextY += SnakeSquareSize;
+                    rowCounter ++;
+                    nextIsOdd = (rowCounter % 2 != 0);
+                }
+
+                if(nextY >=  GameArea.ActualHeight)
+                {
+                    doneDrwaingBackround = true;
+                }
+            }
+        }
+    }
+}
